2a383a97ea8135268ff5155a876bf8579aebc413
Niaolan27 <jasonniow.work@gmail.com>
2023-05-29 13:48:26 +0800

    edited transforms compose

---
+++ reID/transforms.py
@@ -0,0 +1,12 @@
+from torchvision import transforms
+
+class Transforms:
+    def __init__(self):
+        #series of transformations to apply
+        self.transform = transforms.Compose([transforms.Resize((105,105)),
+                                     transforms.ToTensor(),
+                                    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+                                    ]
+                                    )
+    def __call__(self, image):
+        return self.transform(image)
